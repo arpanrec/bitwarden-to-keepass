496c1535b8f091f0676f3c73bdf306b3addbe5d1
arpan <arpan.rec@gmail.com>
2024-11-11 23:53:08 +0530

    Refactor subprocess command execution for improved readability and add security comments

--- src/bitwarden_exporter/__main__.py
+++ src/bitwarden_exporter/__main__.py
@@ -5,7 +5,7 @@ import argparse
 import json
 import logging
 import os.path
-import subprocess
+import subprocess  # nosec B404
 import time
 from typing import Dict, List, Optional
 
@@ -23,7 +23,9 @@ def bw_exec(cmd: List[str], ret_encoding: str = "UTF-8", env_vars: Optional[Dict
     if env_vars is not None:
         cli_env_vars.update(env_vars)
     print(f"Executing CLI :: {' '.join(cmd)}")
-    command_out = subprocess.run(cmd, capture_output=True, check=False, encoding=ret_encoding, env=cli_env_vars)
+    command_out = subprocess.run(
+        cmd, capture_output=True, check=False, encoding=ret_encoding, env=cli_env_vars
+    )  # nosec B603
     if len(command_out.stderr) > 0:
         raise BitwardenException(f"Error in executing command {command_out.stderr}")
     command_out.check_returncode()
